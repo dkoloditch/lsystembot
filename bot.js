bd04fb002ddf6601808e301cd75dc9657f413316
Derek Koloditch <derek.koloditch@gmail.com>
2019-10-14 23:24:05 -0600

    bot improved to mdf hash system params for filename

--- bot.js
+++ bot.js
@@ -1,41 +1,43 @@
-var fs = require('fs');
+const fs = require('fs')
+const crypto = require('crypto')
 
-var generator = require('./generator.js');
-var lsystem = require('./lsystem.js');
-// var twitterer = require('./twitterer.js');
-var mentionHandler = require('./mentionHandler.js');
-// twitterer.useCreds(JSON.parse(fs.readFileSync('./creds.json')));
+const generator = require('./generator.js')
+const lsystem = require('./lsystem.js')
+// const twitterer = require('./twitterer.js')
+// const mentionHandler = require('./mentionHandler.js')
+// twitterer.useCreds(JSON.parse(fs.readFileSync('./creds.json')))
 
-var action, retry;
+const action = function () {
+  const system = generator.generate()
+  const canvasBuf = lsystem.expand(system, 10)
 
-action = function() {
-    var system = generator.generate();
-    var canvasBuf = lsystem.expand(system, 10);
+  if (canvasBuf === null) {
+    console.log('path not good enough, retrying...')
+    retry()
+    return
+  }
 
-    if(canvasBuf === null) {
-        console.log('path not good enough, retrying...');
-        retry();
-        return;
-    }
+  const systemString = JSON.stringify(system)
+  const hash = crypto.createHash('md5').update(systemString).digest('hex')
+  const filename = `${__dirname}/lsystem_result_${hash}.png`
+  fs.writeFileSync(filename, canvasBuf)
 
-    fs.writeFileSync(__dirname + '/text.png', canvasBuf);
+  // console.log('tweeting:', systemString)
 
-    console.log('tweeting:', JSON.stringify(system));
+  // twitterer.tweet(systemString, canvasBuf, undefined, function(error, res) {
+  //    if(error || (res||{}).statusCode !== 200) {
+  //         console.log('error tweeting:', error, (res||{}).body)
+  //        retry()
+  //    } else {
+  //        console.log('tweet success')
+  //    }
+  // })
 
-    // twitterer.tweet(JSON.stringify(system), canvasBuf, undefined, function(error, res) {
-    //    if(error || (res||{}).statusCode !== 200) {
-    //         console.log('error tweeting:', error, (res||{}).body);
-    //        retry();
-    //    } else {
-    //        console.log('tweet success');
-    //    }
-    // });
-
-    // mentionHandler.handleMentions(twitterer);
+  // mentionHandler.handleMentions(twitterer)
 }
 
-retry = function() {
-    setTimeout(action, 1);
+const retry = function () {
+  setTimeout(action, 1)
 }
 
-action();
+action()
