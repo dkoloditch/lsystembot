abbe13d8d5957663f16a2e9d87dc01baadad4220
Derek Koloditch <derek.koloditch@gmail.com>
2019-10-15 22:14:35 -0600

    server sending download and deleting file

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const crypto = require('crypto')
 const generator = require('./generator.js')
 const lsystem = require('./lsystem.js')
 
-const action = function () {
+const bot = async function () {
   const system = generator.generate()
   const canvasBuf = lsystem.expand(system, 10)
 
@@ -16,14 +16,15 @@ const action = function () {
 
   const systemString = JSON.stringify(system)
   const hash = crypto.createHash('md5').update(systemString).digest('hex')
-  const filename = `${__dirname}/lsystem_result_${hash}.png`
-  fs.writeFileSync(filename, canvasBuf)
+  const filePath = __dirname
+  const fileName = `/lsystem_result_${hash}.png`
+  const fullFilePath = `${filePath}${fileName}`
+  await fs.writeFileSync(fullFilePath, canvasBuf)
+  return fullFilePath
 }
 
 const retry = function () {
-  setTimeout(action, 1)
+  setTimeout(bot, 1)
 }
 
-action()
-
-module.exports = action
+module.exports = bot
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,24 +9,32 @@ const bot = require('./bot')
 
 app.use(express.static(path.join(__dirname, '/')))
 
-app.get('/', (req, res) => {
-  const authHeader = req.headers.authorization
-  const bearerPresent = new RegExp(/Bearer \w*/).test(authHeader)
-  if (!bearerPresent) {
-    console.log('Error: Bearer not present. Auth Header:', authHeader)
-    return res.sendStatus(401)
-  }
+app.get('/', async (req, res) => {
+  try {
+    const token = req.query.token
+    if (!token) {
+      console.log('Error: Token not present. Auth Header:', token)
+      return res.sendStatus(401)
+    }
 
-  const token = authHeader.replace('Bearer ', '')
-  if (token !== process.env.SECRET_KEY) {
-    console.log('Error: Secret key does not match. Key:', token)
-    return res.sendStatus(401)
-  }
+    if (token !== process.env.SECRET_KEY) {
+      console.log('Error: Secret key does not match. Key:', token)
+      return res.sendStatus(401)
+    }
 
-  // do the thing
-  console.log('Successful request. Processing...')
-  bot()
-  res.sendStatus(200)
+    console.log('Successful request. Processing...')
+    const fullFilePath = await bot()
+    return await res.download(fullFilePath, error => {
+      if (error) {
+        console.log('Download error:', error)
+      } else {
+        fs.unlinkSync(fullFilePath)
+      }
+    })
+  } catch (error) {
+    console.log('Server Error:', error)
+    return res.send(500)
+  }
 })
 
 app.listen(port, function () {
